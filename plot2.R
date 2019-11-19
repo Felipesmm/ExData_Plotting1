188f0b3af1fdd0686422aef5f9a074c0159c89ea
Felipesmm <felipe.m02@gmail.com>
2019-11-18 21:35:33 -0300

    Changing weekdays to English

--- plot2.R
+++ plot2.R
@@ -1,5 +1,8 @@
-#Load data
+#Load data (It is created in create_data.R)
 econsumption<-readRDS("econsumption.rds")
+
+#Change Region (I have spanish weekdays)
+Sys.setlocale("LC_TIME", "English")
 
 #Create plot
 png(file="plot2.png")
